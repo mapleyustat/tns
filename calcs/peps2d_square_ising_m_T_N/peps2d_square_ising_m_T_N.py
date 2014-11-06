5a5aceaf3ffa8391b43f550b6183891a04d5207d
kingfisher1337 <m.rader@liwest.at>
2014-11-06 19:35:25 +0100

    added m_T_N-calculation using tns-contraction

--- calcs/peps2d_square_ising_m_T_N/peps2d_square_ising_m_T_N.py
+++ calcs/peps2d_square_ising_m_T_N/peps2d_square_ising_m_T_N.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov  6 17:08:43 2014
+Created on Thu Nov  6 19:29:42 2014
 
 @author: michael
 """
@@ -17,29 +17,30 @@ if len(sys.argv) < 3:
     print "  note: number of T-points is 7 + 6*x"
     print "  note: will fork numProcs processes"
     exit(-1)
-
+    
 startTime = time.time()
 
 Nv = 5
-Nh = 20
+Nh = [5, 10, 20, 40]
+NNh = len(Nh)
+
 NT = 7 + int(sys.argv[1])*6
 T = np.linspace(0, 6, NT)
-NH = 3
-H = np.linspace(0, 0.2, NH)
+H = 0.1
 
 numProcs = int(sys.argv[2])
-numDataPointsToProc = -(-(NT*NH) / numProcs) # int-division with ceil
+numDataPointsToProc = -(-(NT*NNh) / numProcs) # int-division with ceil
 
 os.system("mkdir -p tmp")
-os.system("rm tmp/m_T_H_*.dat")
-os.system("rm tmp/chi_T_H_*.dat")
+os.system("rm tmp/m_T_N_*.dat")
+os.system("rm tmp/chi_T_N_*.dat")
 
 for i in range(numProcs):
     if os.fork() == 0:
         firstDataPoint = i*numDataPointsToProc
         lastDataPoint = (i+1)*numDataPointsToProc
-        if lastDataPoint > NT * NH:
-            lastDataPoint = NT * NH
+        if lastDataPoint > NT * NNh:
+            lastDataPoint = NT * NNh
         if lastDataPoint <= firstDataPoint:
             print "nothing left for child process", i
             exit()
@@ -47,25 +48,26 @@ for i in range(numProcs):
         print "child process", i, "doing", (lastDataPoint-firstDataPoint), "data points"
         
         outpidsuffix = "_" + str(i).zfill(2)
-        f2 = open("tmp/m_T_H" + outpidsuffix + ".dat", "w")
-        f3 = open("tmp/chi_T_H" + outpidsuffix + ".dat", "w")
+        f2 = open("tmp/m_T_N" + outpidsuffix + ".dat", "w")
+        f3 = open("tmp/chi_T_N" + outpidsuffix + ".dat", "w")
         
         for j in range(firstDataPoint, lastDataPoint):
-            tns = tnslib.peps2d.square.ising.create(T[j%NT], H[j/NT], BC.periodicBounds, BC.openBounds, Nv, Nh)
+            tns = tnslib.peps2d.square.ising.create(T[j%NT], H, BC.periodicBounds, BC.openBounds, Nv, Nh[j/NT])
+            #chi, m, F = tns.susceptibilityThermodynamic()
             chi, m = tns.susceptibilityInnermost()
-            f2.write(str(H[j/NT]) + " " + str(T[j%NT]) + " " + str(m) + "\n")
-            f3.write(str(H[j/NT]) + " " + str(T[j%NT]) + " " + str(chi) + "\n")
+            f2.write(str(Nh[j/NT]) + " " + str(T[j%NT]) + " " + str(m) + "\n")
+            f3.write(str(Nh[j/NT]) + " " + str(T[j%NT]) + " " + str(chi) + "\n")
         
         f2.close()
         f3.close()
         exit()
-        
+
 for i in range(numProcs):
     os.wait()
 
 print "runtime:", time.time() - startTime, "seconds"
 
-os.system("cat tmp/m_T_H_*.dat > m_T_H.dat")
-os.system("cat tmp/chi_T_H_*.dat > chi_T_H.dat")
-os.system("python plot.py " + str(Nv) + " " + str(Nh) + " " + str(NT) + " " + str(NH))
+os.system("cat tmp/m_T_N_*.dat > m_T_N.dat")
+os.system("cat tmp/chi_T_N_*.dat > chi_T_N.dat")
+os.system("python plot.py " + str(Nv) + " " + str(H) + " " + str(NT) + " " + str(NNh))
 exit()
--- calcs/peps2d_square_ising_m_T_N/plot.py
+++ calcs/peps2d_square_ising_m_T_N/plot.py
@@ -1,39 +1,45 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  6 14:41:31 2014
+
+@author: michael
+"""
 
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
 Nv = int(sys.argv[1])
-Nh = int(sys.argv[2])
+H = float(sys.argv[2])
 NT = int(sys.argv[3])
-NH = int(sys.argv[4])
+NNh = int(sys.argv[4])
 T = np.ndarray(NT)
-H = np.ndarray(NH)
-x = np.ndarray((NH, NT))
+Nh = np.ndarray(NNh)
+x = np.ndarray((NNh, NT))
 
-with open("m_T_H.dat", "r") as f:
+with open("m_T_N.dat", "r") as f:
     i = 0
     for line in f:
         fields = line.split(" ")
         if i < NT:
             T[i] = float(fields[1])
         if i % NT == 0:
-            H[i/NT] = float(fields[0])
+            Nh[i/NT] = float(fields[0])
         x[i/NT,i%NT] = float(fields[2])
         i += 1
+
 plt.clf()
-for i in range(NH):
-    plt.plot(T, x[i], marker="x", label="$H = " + str(H[i]) + "$")
+for i in range(NNh):
+    plt.plot(T, x[i], marker="x", label="$N_h = " + str(Nh[i]) + "$")
 plt.grid(True)
-plt.title("$N_v = " + str(Nv) + "$, $N_h = " + str(Nh) + "$")
+plt.title("$N_v = " + str(Nv) + "$, $H = " + str(H) + "$")
 plt.legend(loc=1)
 plt.xlabel("$T$ $[J]$")
 plt.ylabel("$m = \\langle \\sigma_z^{(i)} \\rangle$")
 plt.ylim(0, 1)
-plt.savefig("m_T_H.png")
+plt.savefig("m_T_N.png")
 
-with open("chi_T_H.dat", "r") as f:
+with open("chi_T_N.dat", "r") as f:
     i = 0
     for line in f:
         fields = line.split(" ")
@@ -41,12 +47,11 @@ with open("chi_T_H.dat", "r") as f:
         i += 1
 
 plt.clf()
-for i in range(NH):
-    plt.plot(T, x[i], marker="x", label="$H = " + str(H[i]) + "$")
+for i in range(NNh):
+    plt.plot(T, x[i], marker="x", label="$N_h = " + str(Nh[i]) + "$")
 plt.grid(True)
-plt.title("$N_v = " + str(Nv) + "$, $N_h = " + str(Nh) + "$")
+plt.title("$N_v = " + str(Nv) + "$, $H = " + str(H) + "$")
 plt.legend(loc=1)
 plt.xlabel("$T$ $[J]$")
 plt.ylabel("$\\chi = \\frac{\partial m}{\partial H}$ $[1/J]$")
-plt.ylim(0, 3.5)
-plt.savefig("chi_T_H.png")
+plt.savefig("chi_T_N.png")
